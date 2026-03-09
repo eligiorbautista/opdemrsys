6411e5542bc1389052697d700a28aafee375100a
elidev <eligiobautista.dev@gmail.com>
2026-03-09 14:20:50 +0800

    fix: add postinstall script for Prisma and lazy DB connection

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,11 +1,7 @@
 const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient({
-  log: ['query', 'error', 'warn']
+  log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error']
 });
-
-prisma.$connect()
-  .then(() => console.log('Database connected'))
-  .catch((error) => console.error('Database connection error:', error));
 
 module.exports = prisma;
